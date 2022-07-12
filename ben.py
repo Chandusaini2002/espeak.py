0cc5943ac3274c12da835b8a4d374299e78628ba
Chandusaini2002 <98374607+Chandusaini2002@users.noreply.github.com>
2022-07-12 17:51:14 +0530

    Add files via upload

--- ben.py
+++ ben.py
@@ -1,40 +1,28 @@
+import subprocess
 import os
-os.system ("espeak 'Hello ,sir'")
-who = input ()                                                                                                        if who == 'who are you':
-    os.system ("espeak 'I am Ben'")
-    os.system ("espeak 'How can help you'")
 
-
-continues = input ("open google to press -g / play song to perss -p " )
-
-
-if continues == 'g':
-    os.system ("espeak ' opening google'")
-    os.system ("w3m 'google.com'")
-
-if continues == 'p':
-    os.system ("espeak 'opening  playing  list'")
-    os.system ("espeak 'choose a song' ")
-    print ("Bad liar -b")
-    print ("Fly -f")
-    print ("Desi_kalakaar -d")
-    print ("Psycho -p")
-
-
-play = input ()
-if play == 'b':
-    os.system ("play 'Bad_Liar.mp3'")
-if play == 'f':
-    os.system ("play 'Fly.mp3'")
-if play == 'd':
-    os.system ("play 'Desi_Kalakaar.mp3'")
-if play == 'p':
-    os.system ("play 'Psycho.mp3'")
-
-
-
-else :
-    os.system ("espeak 'Thank you'")
-    os.system ("cmatrix")
-    os.system ("exit")
-    os.system ("logout")
+record = 1
+while record < 50:
+    i = subprocess.getoutput("termux-speech-to-text")
+    record = record + 1 
+    print (i)
+    if i == 'hello':
+        os.system("espeak 'hello sir' -s 100")
+    elif i == 'hai':
+        os.system("espeak 'ooo you call me' -s 100")
+    elif i == 'good morning':
+        os.system("espeak 'good moring have a very nice bay' -s 100")
+    elif i == 'what is your name':
+        os.system("espeak 'i am termux assistant' -s 100")
+    elif i == 'name':
+        os.system("espeak 'i am just your termux' -s 100")
+    elif i == 'open youtube':
+        os.system("espeak 'opening youtube' -s 100")
+        os.system("termux-open https://m.youtube.com")
+    elif i == 'open google':
+        os.system("espeak 'opening google' -s 100")
+        os.system("termux-open https://www.google.co.in/")
+    elif i == 'stop':
+        os.system("espeak 'ok sir bye' -s 100")
+        break 
+os.system("exit")      
